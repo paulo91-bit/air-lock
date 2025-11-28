74962335dea43f8a3bbfb2cf1635c170dae5c0ed
paulo <mediapaulo91@gmail.com>
2025-11-28 15:17:50 +0100

    testing git workflow

--- risky_code.py
+++ risky_code.py
@@ -3,6 +3,8 @@ import os
 def database_connect():
     # TODO: Remove this before production
     db_password = "super_secret_password_123" 
+    aws_key = "AKIA_FAKE_KEY_12345"
+    print(f"Using AWS Key: {aws_key}")
     print(f"Connecting with {db_password}")
     
     # Infinite loop bug
